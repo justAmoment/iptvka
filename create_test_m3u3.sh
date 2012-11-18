5fdc070c425c6067ef276d86e333ffc0c98eb927
justAmoment <justAmoment@mail.ru>
2012-11-19 02:20:44 +0900

    Initial commit.

---
+++ create_test_m3u3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+x="http://192.168.0.218:4022/udp/"
+b="base"
+#echo "#test" > test3.m3u
+echo "#EXTM3U" > m3u/all.m3u
+for y in `ls $b | sort -n`
+do
+ for i in `ls $b/$y | sort -n`
+ do
+# echo $x$i":5000" >> test3.m3u
+ echo "#EXTINF:-1, "$i" -- "`head -1 $b/$y/$i` >> m3u/all.m3u
+ echo $x$y.$i":5000" >> m3u/all.m3u
+ done;
+done;
