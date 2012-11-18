e6a4a7fdd31dbee9191effd257701f1004ea8431
justAmoment <justAmoment@mail.ru>
2012-11-19 04:57:09 +0900

    Add format/[port,prefix_ip,raw].

--- create_test_m3u3.sh
+++ create_test_m3u3.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-x="http://192.168.0.218:4022/udp/"
-b="base"
-cat format/head > m3u/all.m3u
-for y in `ls $b | sort -n`
+dir_base="base"
+dir_format="format"
+x=`cat $dir_format"/prefix_ip"`
+port_iptv=`cat $dir_format"/port"`
+cat $dir_format"/head" > m3u/all.m3u
+for y in `ls $dir_base | sort -n`
 do
- for i in `ls $b/$y | sort -n`
+ for i in `ls $dir_base/$y | sort -n`
  do
- echo "#EXTINF:-1, "$i" -- "`head -1 $b/$y/$i` >> m3u/all.m3u
- echo $x$y.$i":5000" >> m3u/all.m3u
+ echo "#EXTINF:-1, "$i" -- "`head -1 $dir_base/$y/$i` >> m3u/all.m3u
+ echo $x$y.$i":"$port_iptv >> m3u/all.m3u
  done;
 done;
