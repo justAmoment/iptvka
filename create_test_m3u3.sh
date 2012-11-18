03e1ba6013031cea0fb680c4c22f956148ec2fa5
justAmoment <justAmoment@mail.ru>
2012-11-19 05:20:28 +0900

    Switch to use target_m3u, format/raw.

--- create_test_m3u3.sh
+++ create_test_m3u3.sh
@@ -2,13 +2,16 @@
 dir_base="base"
 dir_format="format"
 x=`cat $dir_format"/prefix_ip"`
+r=`cat $dir_format"/raw"`
 port_iptv=`cat $dir_format"/port"`
-cat $dir_format"/head" > m3u/all.m3u
+target_m3u="m3u/all.m3u"
+
+cat $dir_format"/head" > $target_m3u
 for y in `ls $dir_base | sort -n`
 do
  for i in `ls $dir_base/$y | sort -n`
  do
- echo "#EXTINF:-1, "$i" -- "`head -1 $dir_base/$y/$i` >> m3u/all.m3u
- echo $x$y.$i":"$port_iptv >> m3u/all.m3u
+ echo $r" "$i" -- "`head -1 $dir_base/$y/$i` >> $target_m3u
+ echo $x$y.$i":"$port_iptv >> $target_m3u
  done;
 done;
