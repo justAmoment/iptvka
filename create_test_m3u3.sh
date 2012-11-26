cc31ecd9e451a27b3ac596b5626d3cb79b64e14d
justAmoment <justAmoment@mail.ru>
2012-11-27 00:22:53 +0900

    Add var 'type' for 'igmp udpxy'. Reformat code.

--- create_test_m3u3.sh
+++ create_test_m3u3.sh
@@ -1,50 +1,43 @@
 #!/bin/sh
 provider="rtk"
-dir_provider="provider/"$provider
+dir_provider="provider/"${provider}
 dir_format="format"
 dir_list="list"
-x_i=`cat $dir_format"/prefix_ip_igmp"`
-x_u=`cat $dir_format"/prefix_ip_udpxy"`
-r=`cat $dir_format"/raw"`
+r=`cat ${dir_format}"/raw"`
+types="igmp udpxy"
 
-# Generate all lists
-target_m3u_i="m3u/iptv_"$provider"_igmp_all.m3u"
-target_m3u_u="m3u/iptv_"$provider"_udpxy_all.m3u"
-
-cat $dir_format"/head" > $target_m3u_i
-cat $dir_format"/head" > $target_m3u_u
-for port in `ls $dir_provider | sort -n`
+for type in ${types}
 do
- for y in `ls $dir_provider"/"$port | sort -n`
+ x=`cat ${dir_format}"/prefix_ip_"${type}`
+
+ # Generate all lists
+ target_m3u="m3u/iptv_"${provider}"_"${type}"_all.m3u"
+ cat ${dir_format}"/head" > ${target_m3u}
+ for port in `ls ${dir_provider} | sort -n`
  do
-  for i in `ls $dir_provider"/"$port"/"$y | sort -n`
+  for y in `ls ${dir_provider}"/"${port} | sort -n`
   do
-   echo $r""`sed -n 3,3p $dir_provider"/"$port"/"$y"/"$i`", "$i" -- "`sed -n 1,1p $dir_provider"/"$port"/"$y"/"$i` >> $target_m3u_i
-   echo $r""`sed -n 3,3p $dir_provider"/"$port"/"$y"/"$i`", "$i" -- "`sed -n 1,1p $dir_provider"/"$port"/"$y"/"$i` >> $target_m3u_u
-   echo ${x_i}$y.$i":"$port >> $target_m3u_i
-   echo ${x_u}$y.$i":"$port >> $target_m3u_u
+   for i in `ls ${dir_provider}"/"${port}"/"${y} | sort -n`
+   do
+    echo ${r}""`sed -n 3,3p ${dir_provider}"/"${port}"/"${y}"/"${i}`", "${i}" -- "`sed -n 1,1p ${dir_provider}"/"${port}"/"${y}"/"${i}` >> ${target_m3u}
+    echo ${x}${y}"."${i}":"${port} >> ${target_m3u}
+   done
   done
  done
-done
-# Generate special lists
-for list in `ls $dir_list`
-do
- target_m3u_i="m3u/iptv_"$provider"_igmp_"$list".m3u"
- target_m3u_u="m3u/iptv_"$provider"_udpxy_"$list".m3u"
- cat $dir_format"/head" > $target_m3u_i
- cat $dir_format"/head" > $target_m3u_u
 
- for x in `cat $dir_list"/"$list`
+ # Generate special lists
+ for list in `ls ${dir_list}`
  do
-  ary=(${x//./ })
-
-  ip123=${ary[0]}"."${ary[1]}"."${ary[2]}
-  ip4=${ary[3]}
-  port=${ary[4]}
-  echo $r""`sed -n 3,3p $dir_provider"/"$port"/"$ip123/$ip4`", "$ip4" -- "`sed -n 1,1p $dir_provider"/"$port"/"$ip123/$ip4` >> $target_m3u_i
-  echo $r""`sed -n 3,3p $dir_provider"/"$port"/"$ip123/$ip4`", "$ip4" -- "`sed -n 1,1p $dir_provider"/"$port"/"$ip123/$ip4` >> $target_m3u_u
-  echo ${x_i}$ip123"."$ip4":"$port >> $target_m3u_i
-  echo ${x_u}$ip123"."$ip4":"$port >> $target_m3u_u
-
+  target_m3u="m3u/iptv_"${provider}"_"${type}"_"${list}".m3u"
+  cat ${dir_format}"/head" > ${target_m3u}
+  for la in `cat ${dir_list}"/"${list}`
+  do
+   ary=(${la//./ })
+   ip123=${ary[0]}"."${ary[1]}"."${ary[2]}
+   ip4=${ary[3]}
+   port=${ary[4]}
+   echo ${r}""`sed -n 3,3p ${dir_provider}"/"${port}"/"${ip123}/${ip4}`", "${ip4}" -- "`sed -n 1,1p ${dir_provider}"/"${port}"/"${ip123}/${ip4}` >> ${target_m3u}
+   echo ${x}${ip123}"."${ip4}":"${port} >> ${target_m3u}
+  done
  done
 done
