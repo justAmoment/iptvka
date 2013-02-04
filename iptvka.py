d44e5c589f74de6ab65176081d3aa110c2d7f8ec
justAmoment <justAmoment@mail.ru>
2013-02-05 02:31:30 +0900

    Use 'ConfigParser' to set value 'dir_from' from config file.

--- iptvka.py
+++ iptvka.py
@@ -1,11 +1,23 @@
 #!/bin/python
 
+import ConfigParser, os
 from gi.repository import Gtk, Gdk
 from iptvka_gui import iptvkaWindow
 
-dir_from = "."
+f_cfg = 'iptvka.cfg'
+config = ConfigParser.ConfigParser()
+try:
+    config.readfp(open(f_cfg))
+    dir_from = str(config.get("general", "dir_from"))
+except:
+    dir_from = "."
 
-window = iptvkaWindow(dir_from = dir_from)
+try:
+    window = iptvkaWindow(dir_from = dir_from)
+except:
+    print "'dir_from' has a wrong value = %s. Please check the config file '%s'. Now 'dir_from' set to the current directory." % (dir_from, f_cfg)
+    dir_from = "."
+    window = iptvkaWindow(dir_from = dir_from)
 window.connect("delete-event", Gtk.main_quit)
 window.show_all()
 
