adefb9f53e5d3e1de22f650c1de230ba33c5ecb7
justAmoment <justAmoment@mail.ru>
2013-02-05 03:58:55 +0900

    Set name of the config file from 'sys.argv[1]'.

--- iptvka.py
+++ iptvka.py
@@ -1,10 +1,14 @@
 #!/bin/python
 
-import ConfigParser, os
+import ConfigParser, sys
 from gi.repository import Gtk, Gdk
 from iptvka_gui import iptvkaWindow
 
-f_cfg = 'iptvka.cfg'
+if len(sys.argv) > 1:
+    f_cfg = sys.argv[1]
+else:
+    f_cfg = 'iptvka.cfg'
+
 config = ConfigParser.ConfigParser()
 try:
     config.readfp(open(f_cfg))
@@ -15,7 +19,7 @@ except:
 try:
     window = iptvkaWindow(dir_from = dir_from)
 except:
-    print "'dir_from' has a wrong value = %s. Please check the config file '%s'. Now 'dir_from' set to the current directory." % (dir_from, f_cfg)
+    print "'dir_from' has a wrong value = '%s'. Please check the config file '%s'. Now 'dir_from' set to the current directory." % (dir_from, f_cfg)
     dir_from = "."
     window = iptvkaWindow(dir_from = dir_from)
 window.connect("delete-event", Gtk.main_quit)
