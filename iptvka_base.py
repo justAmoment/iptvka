93ec068ddfb166243825d9790281d5e55be4a7df
justAmoment <justAmoment@mail.ru>
2013-02-11 03:34:31 +0900

    New file 'iptvka_base.py'.

---
+++ iptvka_base.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+import ConfigParser, sys, os
+
+class iptvkaBase():
+    """Class stores inner vars, functions to interact with file system and inner vars."""
+    dir_from = "."
+    dir_prov = "provider"
+    dir_format = "format"
+    dir_m3u = "m3u"
+    f_cfg = "iptvka.cfg"
+
+    def __init__(self, dir_from):
+        self.dir_from = dir_from
+        if len(sys.argv) > 1:
+            self.f_cfg = sys.argv[1]
+        else:
+            self.f_cfg = os.path.join(self.dir_from, "iptvka.cfg")
+
+        config = ConfigParser.ConfigParser()
+        try:
+            config.readfp(open(self.f_cfg))
+            self.dir_from = str(config.get("general", "dir_from"))
+            if self.dir_from == ".":
+                self.dir_from = os.path.realpath(os.path.dirname(__file__))
+            self.dir_prov = str(config.get("general", "dir_prov"))
+            self.dir_format = str(config.get("general", "dir_format"))
+            self.dir_m3u = str(config.get("general", "dir_m3u"))
+        except:
+            pass
